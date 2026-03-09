cbe30f214d151ba710cc1a0448c1ea9f7623e4ef
shin jaewon <itrust67709901@gmail.com>
2026-03-09 23:06:58 +0900

    Update predict.py

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -1,32 +1,33 @@
-from fastapi import APIRouter, UploadFile, File,HTTPException
+from fastapi import APIRouter, UploadFile, File
 from app.services.inference_service import run_inference
 from app.db.database import get_connection
+from app.schemas import PredictionResponse
 
 router = APIRouter()
-
 
 @router.post("/predict", response_model=PredictionResponse)
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
-    result = run_inference(file.filename)
+    result = run_inference(contents, file.filename)
 
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute(
-        """
-        INSERT INTO predictions (image_name, prediction, confidence)
-        VALUES (%s, %s, %s)
-        """,
-        (
-            result["image_name"],
-            result["prediction"],
-            result["confidence"]
+    try:
+        cursor.execute(
+            """
+            INSERT INTO predictions (image_name, prediction, confidence)
+            VALUES (%s, %s, %s)
+            """,
+            (
+                result["image_name"],
+                result["prediction"],
+                result["confidence"]
+            )
         )
-    )
-
-    conn.commit()
-    cursor.close()
-    conn.close()
+        conn.commit()
+    finally:
+        cursor.close()
+        conn.close()
 
     return result
