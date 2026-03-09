2bfeadc8010a9166db86a8d9210165a61f988b36
shin jaewon <itrust67709901@gmail.com>
2026-03-09 22:25:13 +0900

    Update db.py

--- app/services/db.py
+++ app/services/db.py
@@ -1,1 +1,11 @@
+import os
+import mysql.connector
 
+def get_connection():
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+    return conn
