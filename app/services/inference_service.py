588b5fe20e1826f12555e14f96683ca610c5afcd
shin jaewon <itrust67709901@gmail.com>
2026-03-09 23:04:54 +0900

    Update inference_service.py

--- app/services/inference_service.py
+++ app/services/inference_service.py
@@ -1,8 +1,8 @@
 def run_inference(file_bytes: bytes, file_name: str):
-    # 실제로는 모델 추론 결과를 반환
+    # 더미코드로 실제로는 모델 추론 결과를 반환하는 것을 사용
     prediction = "defect"
     confidence = 0.91
-
+  
     return {
         "image_name": file_name,
         "prediction": prediction,
