fde99e7c1645202cb223d35a71ef661f2631700d
shin jaewon <itrust67709901@gmail.com>
2026-03-10 11:49:24 +0900

    Update inference_service.py

--- app/services/inference_service.py
+++ app/services/inference_service.py
@@ -1,7 +1,39 @@
-def run_inference(file_bytes: bytes, file_name: str):
-    # 나중에 실제 모델추론 결과로 대체
-    prediction = "defect"
-    confidence = 0.91
+from io import BytesIO
+import numpy as np
+from PIL import Image
+
+from app.services.model_loader import load_model
+
+
+def preprocess_image(file_bytes):
+
+    image = Image.open(BytesIO(file_bytes))
+
+    image = image.convert("RGB")
+
+    image = image.resize((256, 256))
+
+    image = np.array(image) / 255.0
+
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+
+def run_inference(file_bytes, file_name):
+
+    model = load_model()
+
+    input_image = preprocess_image(file_bytes)
+
+    pred = model.predict(input_image)
+
+    confidence = float(pred[0][0])
+
+    if confidence >= 0.5:
+        prediction = "defect"
+    else:
+        prediction = "normal"
 
     return {
         "image_name": file_name,
